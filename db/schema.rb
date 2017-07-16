f78f9731a5c9dd69781131b47fce9a8128f52e6c
checkashu <ashutosh113163@nitp.ac.in>
2017-07-16 16:56:40 +0000

    temp

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170715153129) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "applications", force: :cascade do |t|
     t.string   "address"
     t.string   "applicant"
